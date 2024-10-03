4fd87f5a07d3f30584adb4821c1f4d5f20479949
Eremin-jpg <byeshyenayaytka@gmail.com>
2024-10-03 23:21:37 +0300

    В программе изменена функция

--- main.c
+++ main.c
@@ -1,28 +1,24 @@
-#include <stdio.h>
 #include <math.h>
-#define A -5.0 
-#define B 5.0 
-#define H 0.1 
+#include <stdio.h>
+#define A -10.0
+#define B 10.0
+#define H 0.1
 
 /* Функция */
-double F(double x)
-{
- return 40 * x * exp(x) + 2 * sin(x) + sqrt(fabs(pow(x, 3) - pow(x, 2)));
+double F(double x) {
+  return 40 * x * exp(2 * x) + 2 * sin(x) + sqrt(fabs(pow(x, 3) - pow(x, 2)));
 }
 
-int main( )
-{
-double x, y, sum;
-sum = 0;
-for(x = A; x <= B; x += H)
-  {
-    y = F(x); 
-    printf("%10.4f %10.4f\n", x, y); 
-    if (x > y)
-      {
-        sum += y;
-      }
+int main() {
+  double x, y, sum;
+  sum = 0;
+  for (x = A; x <= B; x += H) {
+    y = F(x);
+    printf("%10.4f %10.4f\n", x, y);
+    if (x > y) {
+      sum += y;
+    }
   }
-printf("x > f(x) = %10.4f\n", sum);
-return 0;
+  printf("x > f(x) = %10.4f\n", sum);
+  return 0;
 }
